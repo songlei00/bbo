4225836e393f71777072eaf19ceff8f7f54e6ef3
s974534426 <974534426@qq.com>
2023-11-09 16:00:49 +0800

    init

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_namespace_packages
+
+
+def _get_version():
+    with open('bbo/__init__.py') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                g = {}
+                exec(line, g)
+                return g['__version__']
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='bbo',
+    version=_get_version(),
+    description='Black-box optimization library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='songl',
+    author_email='songl@lamda.nju.edu.cn',
+    url='https://github.com/songlei00/bbo',
+    packages=find_namespace_packages(include=['bbo*']),
+    include_package_data=True,
+    install_requires=required,
+)
